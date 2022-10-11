4d0c6242a1d2b9233e801757a95d7f583da0a1fe
canasmh <canasmh@yahoo.com>
2022-10-11 13:38:28 -0400

    Use axios to make a get request on the APOD api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react"
+import Header from "./Header"
+import ButtonApod from "./buttonAPOD"
+import axios from "axios";
+import "./App.css"
+
+const APIKey = "ZZiO6f1RcheyeRmwgte17PfJTpALOaTUg64Ucy7Z"
 
 function App() {
+
+  const [showAPOD, setShowAPOD] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}`)
+    .then((res) => {
+      setData(res.data);
+    })
+    .catch((err) => {
+      console.log(`There was an error: ${err}`)
+    })
+
+  }, [data])
+
   return (
     <div className="App">
       <p>
